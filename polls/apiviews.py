d236e5a710d2e75559ee27d8ead50edd30f28287
Femi-ID <104504319+Femi-ID@users.noreply.github.com>
2023-10-07 23:55:16 +0100

    Update view.py using generics view

--- polls/apiviews.py
+++ polls/apiviews.py
@@ -2,23 +2,33 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from .models import Poll, Choice
-from .serializers import PollSerializer, ChoiceSerializer
+from .serializers import PollSerializer, ChoiceSerializer, VoteSerializer
 from rest_framework import status
+from rest_framework import generics
 
 
-class PollList(APIView):
-    def get(self, request):
-        polls = Poll.objects.all()
-        # data = PollSerializer(polls, many=True).data
-        serializer = PollSerializer(polls, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+# class PollList(APIView):
+#     def get(self, request):
+#         polls = Poll.objects.all()
+#         # data = PollSerializer(polls, many=True).data
+#         serializer = PollSerializer(polls, many=True)
+#         return Response(serializer.data, status=status.HTTP_200_OK)
 
 
-class PollDetail(APIView):
-    def get(self, request, id):
-        poll = get_object_or_404(Poll, id=id)
-        # data =PollSerializer(poll)
-        serializer = PollSerializer(poll)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+# class PollDetail(APIView):
+#     def get(self, request, id):
+#         poll = get_object_or_404(Poll, id=id)
+#         # data =PollSerializer(poll)
+#         serializer = PollSerializer(poll)
+#         return Response(serializer.data, status=status.HTTP_200_OK)
 
+
+class PollList(generics.ListCreateAPIView):
+    queryset = Poll.objects.all()
+    serializer_class = PollSerializer
+
+
+class PollDetail(generics.RetrieveUpdateAPIView):
+    queryset = Poll.objects.all()
+    serializer_class = PollSerializer
 
